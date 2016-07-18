443ca7403cb32ff211a47c885c3505bb16a20982
Matt Kraatz <matt.kraatz@gmail.com>
2016-07-18 12:55:45 -0500

    initial commit

---
+++ products.js
@@ -0,0 +1,17 @@
+
+console.log("hello");
+
+var products = [
+  a{
+    name: "Toy A",
+    img: "https://unsplash.it/200/300/?random",
+    price: "9.99",
+    descr: "This is not a toy.";
+  },
+  b{
+    name: "Toy B",
+    img: "https://unsplash.it/200/300/?random",
+    price: "9.99",
+    descr: "This is definitely not a toy.";
+  }
+];
